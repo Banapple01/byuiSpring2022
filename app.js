c5abfc442e48815e1b8a79ffa853e9f095b4fe74
Jonathan Wright <wrightjonathan99@gmail.com>
2022-05-13 12:39:20 -0600

    adding swagger

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const indexRoutes = require('./routes/index');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
     })
-    .use('/', indexRoutes);
+    .use('/', indexRoutes)
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ const indexController = require('../controllers/indexController');
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/', indexController.getData);
+// router.get('/', indexController.getData);
 // GET /feed/posts
 // localhost:8080/index/
+router.get('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
